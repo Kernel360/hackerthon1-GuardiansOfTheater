107bc933fd324178059df121521d10b086d500ff
Chan <ieejo716@naver.com>
2023-10-19 15:14:12 +0900

    [FIX] S.O.S

--- project_theater/src/main/java/org/example/springsecurityexample/SpringSecurityExampleApplication.java
+++ project_theater/src/main/java/org/example/springsecurityexample/SpringSecurityExampleApplication.java
@@ -2,7 +2,9 @@ package org.example.springsecurityexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class SpringSecurityExampleApplication {
 
